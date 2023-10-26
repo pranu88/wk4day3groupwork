c69b6ed65585b536a33b7c7ece930920a431857c
pranu88 <pranusha.pothugunta@gmail.com>
2023-10-26 11:30:51 -0400

    yell at the ninja turtles answered

--- ayyaysLoops.js
+++ ayyaysLoops.js
@@ -30,22 +30,34 @@ If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 // =====================
 //    WILD WILD LIFE
 // =====================
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-// 1.Plantee just had her birthday; change Plantee's array to reflect her being a year older.
-plantee[2] = 5001
-console.log(plantee)
-// 2.Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
-// wolfy[3] = "Gotham city"
-wolfy.splice(3,1,"Gotham city")
-console.log(wolfy)
-// 3.Give D'Art a second hometown by adding "Hawkins"
-dart.push('hawkins')
-console.log(dart)
-// 4.Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
-wolfy.splice(0,1,'Gameboy')
-console.log(wolfy)
+// // 1.Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+// plantee[2] = 5001
+// console.log(plantee)
+// // 2.Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// // wolfy[3] = "Gotham city"
+// wolfy.splice(3,1,"Gotham city")
+// console.log(wolfy)
+// // 3.Give D'Art a second hometown by adding "Hawkins"
+// dart.push('hawkins')
+// console.log(dart)
+// // 4.Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+// wolfy.splice(0,1,'Gameboy')
+// console.log(wolfy)
+
+// ================================
+/* Yell at the Ninja Turtles
+// ================================
+Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result.
+As a developer, you'll be a lifelong learner and constantly encountering new things. We'll give you little stretches like this to get you used to looking at documentation and figuring some things out on your own. You've got this!
+*/
+const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
+for(let turtle of ninjaTurtles){
+console.log(turtle.toUpperCase())
+}
