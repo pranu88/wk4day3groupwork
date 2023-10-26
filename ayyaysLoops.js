a2565622913921d09c140dc8c0d570808eddedf6
pranu88 <pranusha.pothugunta@gmail.com>
2023-10-26 10:56:55 -0400

    wild wild life answered

--- ayyaysLoops.js
+++ ayyaysLoops.js
@@ -20,9 +20,32 @@ If a number is divisible by 5 log "Buzz" instead of the number.
 If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 */
 
-for(let i = 1;i <= 100;i++){
-    if(i % 3 === 0 && i % 5 ===0) console.log('FizzBuzz')
-    else if (i % 3 === 0) console.log("Fizz")
-    else if (i % 5 === 0) console.log("Buzz")
-    else console.log(i)
-}
+// for(let i = 1;i <= 100;i++){
+//     if(i % 3 === 0 && i % 5 ===0) console.log('FizzBuzz')
+//     else if (i % 3 === 0) console.log("Fizz")
+//     else if (i % 5 === 0) console.log("Buzz")
+//     else console.log(i)
+// }
+
+// =====================
+//    WILD WILD LIFE
+// =====================
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// 1.Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2] = 5001
+console.log(plantee)
+// 2.Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// wolfy[3] = "Gotham city"
+wolfy.splice(3,1,"Gotham city")
+console.log(wolfy)
+// 3.Give D'Art a second hometown by adding "Hawkins"
+dart.push('hawkins')
+console.log(dart)
+// 4.Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+wolfy.splice(0,1,'Gameboy')
+console.log(wolfy)
